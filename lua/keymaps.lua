719b364b7d13f7ef99f905e4f2dea116d09f968f
Alxhr0 <92590433+Alxhr0@users.noreply.github.com>
2024-03-14 12:44:46 +0100

    Update keymaps.lua

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -11,21 +11,3 @@ vim.keymap.set('n', '<leader><Tab>', '<cmd>bnext<cr>')
 
 vim.keymap.set('n', '<leader>rp', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>') -- Replace all instance of current word in file
 
--- Unset arrow keys
-vim.cmd [[
-    noremap <Left> <Nop>
-    noremap <Right> <Nop>
-    noremap <Up> <Nop>
-    noremap <Down> <Nop>
-
-
-    inoremap <Left> <Nop>
-    inoremap <Right> <Nop>
-    inoremap <Up> <Nop>
-    inoremap <Down> <Nop>
-]]
-
--- Disable Mouse
-vim.cmd [[
-    set mouse=
-]]
