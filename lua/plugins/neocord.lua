711dbc1a4ad69431cba691476e2b57cabaaa530e
Alxhr0 <alxhr0@proton.me>
2024-05-08 14:20:51 +0200

    Change plugins

--- lua/plugins/neocord.lua
+++ lua/plugins/neocord.lua
@@ -2,5 +2,8 @@ return {
   'IogaMaster/neocord',
   config = {
     logo = 'https://raw.githubusercontent.com/IogaMaster/neovim/main/.github/assets/nixvim-dark.webp', -- Main image display (either "neovim" or "file")
+    editing_text = 'Breaking %s',
+    terminal_text = 'Lurking in the Terminal',
+    workspace_text = 'Tinkering with %s',
   },
 }
